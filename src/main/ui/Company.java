8eb2e775247aec9caa05c469a77e6ef3f4d18c6e
dongandy <github.students.ubc.ca>
2019-09-25 00:07:51 -0700

    check 5

--- src/main/ui/Company.java
+++ src/main/ui/Company.java
@@ -26,30 +26,29 @@ public class Company {
         Scanner s = new Scanner(System.in);
         Employees emp = new Employees("v0g2b");
         Visitors vis = new Visitors("Andy");
-        System.out.println("Hello");
-        System.out.println("Are you a visitor?");
+        // System.out.println("Hello");
+        // System.out.println("Are you a visitor?");
+        // s.nextLine();
+        // if (s.equals("Yes")) {
+        vis.greetingVisitors();
         s.nextLine();
-        if (s.equals("Yes")) {
-            vis.greetingVisitors();
-            s.nextLine();
-            CheckIn.visitorStay();
-            c.occupants.add("Andy");
-            c.occupants.add("Andy Jr");
-            CheckOut.visitorLeave();
-            c.occupants.remove("Andy");
-            c.occupants.remove("Andy Jr");
-        } else {
-            emp.greetingEmployees();
-            System.out.print("");
-            s.nextLine();
-            CheckIn.employeeStay();
-            c.occupants.add("Bob");
-            // timer will run
-            // later reminder will activate when shift is almost over
-            Timer t = new Timer(6);
-            t.runningTimer();
-            CheckOut.employeeLeave();
-            c.occupants.remove("Bob");
-        }
+        CheckIn.visitorStay();
+        c.occupants.add("Andy");
+        // c.occupants.add("Andy Jr");
+        CheckOut.visitorLeave();
+        c.occupants.remove("Andy");
+        // c.occupants.remove("Andy Jr");
+        // } else {
+        emp.greetingEmployees();
+        // System.out.print("");
+        // s.nextLine();
+        CheckIn.employeeStay();
+        c.occupants.add("Bob");
+        // timer will run
+        // later reminder will activate when shift is almost over
+        Timer t = new Timer(6);
+        t.runningTimer();
+        CheckOut.employeeLeave();
+        c.occupants.remove("Bob");
     }
 }
